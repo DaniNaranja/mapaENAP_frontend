4b4cefd710623bff75d7f93a189c3f5a27adca67
DaniNaranja <d.paredes04@ufromail.cl>
2025-01-22 16:17:00 -0300

    Se agregan componentes navbar y sidebar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="navbar bg-gradient-to-r from-gray-100 to-violet-200 h-20">
+        <div class="flex-1">
+            <router-link to="/home">
+                <img src="../assets/logo.png" alt="" class="h-20 p-3">
+            </router-link>
+
+        </div>
+        <div class="flex-none gap-4">
+            <div class="dropdown">
+                
+                
+            </div>
+        </div>
+        
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="h-screen flex flex-col w-48 bg-gradient-to-b from-violet-500 to-indigo-800" >
+        <RouterLink to="/mapa">
+            <div class="flex items-center justify-between pr-4 pl-4 pt-2 pb-2 hover:bg-violet-900">
+                        <div class="flex items-center">
+                            <i class="fa-solid fa-map-location-dot m-3 text-2xl" style="color: white"></i>
+                            
+                            <span style="font-size: 16px; color: white; font-weight: 600;">Mapa</span>
+                        </div>
+                        <i class="fas fa-chevron-right text-xl ml-3 arrow"></i>
+                    </div>
+        </RouterLink>
+        <RouterLink to="/permisos">
+            <div class="flex items-center justify-between pr-4 pl-4 pt-2 pb-2 hover:bg-violet-900">
+                        <div class="flex items-center">
+
+                            <i class="fa-solid fa-clipboard-list m-3.5 text-2xl" style="color: white"></i>
+                            <span style="font-size: 16px; color: white; font-weight: 600;">Permisos</span>
+                        </div>
+                        <i class="fas fa-chevron-right text-xl ml-3 arrow"></i>
+                    </div>
+        </RouterLink>
+        <RouterLink to="/historial">
+            <div class="flex items-center justify-between pr-4 pl-4 pt-2 pb-2 hover:bg-violet-900">
+                        <div class="flex items-center">
+
+                            <i class="fa-solid fa-clock-rotate-left m-3 text-2xl" style="color: white"></i>
+                            <span style="font-size: 16px; color: white; font-weight: 600;">Historial</span>
+                        </div>
+                        <i class="fas fa-chevron-right text-xl ml-3 arrow"></i>
+                    </div>
+        </RouterLink>
+        
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  
+}
+</script>
+
+<style scoped>
+.side-nav {
+    width: 300px;
+    transition: width 0.3s ease;
+}
+
+@media (max-width: 768px) {
+    .side-nav {
+        width: 80px;
+    }
+
+    .side-nav span {
+        display: none;
+    }
+}
+
+</style>
